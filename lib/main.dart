e0f104ed0392112e3acea350e0bc896bc5e672e3
Keito Yamakawa <keito.yamakawa@smail.astate.edu>
2025-01-27 10:22:08 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Students Marketplace',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Students Marketplace'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Students Marketplace'),
+      ),
+    );
+  }
+} 
