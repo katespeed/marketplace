8eb0bb57ce51bbd89c65cdff2fbcd569042d7bcb
Keito Yamakawa <keito.yamakawa@smail.astate.edu>
2025-01-28 19:48:42 -0600

    add folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// Description: This code initializes a Flutter application with Firebase.
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/firebase_options.dart';
