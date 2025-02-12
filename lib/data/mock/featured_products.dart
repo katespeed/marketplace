33ec8bf32f5e7c4e6731c56bf96a4c28c1843f4d
Keito Yamakawa <keito.yamakawa@smail.astate.edu>
2025-02-12 14:55:12 -0600

    add feature item list

---
+++ lib/data/mock/featured_products.dart
@@ -0,0 +1,64 @@
+import 'package:my_flutter_app/domain/models/product.dart';
+
+final List<Product> mockFeaturedProducts = [
+  Product(
+    id: '2',
+    name: 'Wireless Speaker',
+    price: 199.99,
+    imageUrl: 'assets/products/speaker.jpeg',
+  ),
+  Product(
+    id: '3',
+    name: 'Smart Watch',
+    price: 249.99,
+    imageUrl: 'assets/products/watch.jpeg',
+  ),
+  Product(
+    id: '4',
+    name: 'Laptop Pro',
+    price: 1299.99,
+    imageUrl: 'assets/products/laptop.jpeg',
+  ),
+    Product(
+    id: '5',
+    name: 'Macbook Pro',
+    price: 199.99,
+    imageUrl: 'assets/products/macbook_pro.jpeg',
+  ),
+  Product(
+    id: '6',
+    name: 'iPone 13',
+    price: 249.99,
+    imageUrl: 'assets/products/iphone_13.jpeg',
+  ),
+  Product(
+    id: '7',
+    name: 'Dyson Vacuum',
+    price: 1299.99,
+    imageUrl: 'assets/products/dyson_vacuum.jpeg',
+  ),
+  Product(
+    id: '8',
+    name: 'Nike Air Max',
+    price: 199.99,
+    imageUrl: 'assets/products/nike_air_max.jpeg',
+  ),
+  Product(
+    id: '9',
+    name: 'IKEA desk',
+    price: 249.99,
+    imageUrl: 'assets/products/ikea_desk.jpeg',
+  ),
+  Product(
+    id: '10',
+    name: 'Samsung TV',
+    price: 1299.99,
+    imageUrl: 'assets/products/samsung_tv.jpeg',
+  ),
+    Product(
+    id: '11',
+    name: 'Canon Camera',
+    price: 499.99,
+    imageUrl: 'assets/products/canon_camera.jpeg',
+  ),
+]; 
--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_flutter_app/presentation/components/appbar/appbar.dart';
 import 'package:my_flutter_app/presentation/components/buttons/custom_button.dart';
+import 'package:my_flutter_app/data/mock/featured_products.dart';
+import 'package:my_flutter_app/presentation/components/grids/featured_products_grid.dart';
 
 class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
@@ -11,34 +14,88 @@ class HomePage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CustomButton(
-              text: 'Proceed to Checkout',
-              onPressed: () {
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.push('/product-list');
-              },
-              child: const Text('Go to Product List'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.push('/payment');
-              },
-              child: const Text('Go to Payment'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.push('/product-detail');
-              },
-              child: const Text('Go to Product Detail'),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 60.0, horizontal: 60.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  constraints: BoxConstraints(
+                    // maxHeight: MediaQuery.of(context).size.height * 0.8,
+                    // minHeight: 100,
+                  ),
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  // height: MediaQuery.of(context).size.height * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  clipBehavior: Clip.antiAlias,
+                  child: SvgPicture.asset(
+                    'assets/home_image.svg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Center(
+                  child: Wrap(
+                    alignment: WrapAlignment.start,
+                    spacing: 20,
+                    runSpacing: 10,
+                    children: [
+                      CustomButton(
+                        backgroundColor: Colors.grey,
+                        text: 'Proceed to Checkout',
+                        onPressed: () {
+                          context.push('/forgot-password');
+                        },
+                      ),
+                      CustomButton(
+                        backgroundColor: Colors.grey,
+                        onPressed: () {
+                          context.push('/product-list');
+                        },
+                        text: 'Go to Product List',
+                      ),
+                      CustomButton(
+                        backgroundColor: Colors.grey,
+                        onPressed: () {
+                          context.push('/payment');
+                        },
+                        text: 'Go to Payment',
+                      ),
+                      CustomButton(
+                        backgroundColor: Colors.grey,
+                        onPressed: () {
+                          context.push('/product-detail');
+                        },
+                        text: 'Go to Product Detail',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Featured Items',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              FeaturedProductsGrid(products: mockFeaturedProducts),
+            ],
+          ),
         ),
       ),
     );
